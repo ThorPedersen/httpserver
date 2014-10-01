c4b30934daeb7da1296ecc24df4a0f36db326745
Roslarb <kristoffer.mikkelsen@hotmail.com>
2014-10-01 14:01:48 +0200

    Oprettet StatusHandling class

--- httpserver/HttpServer.cs
+++ httpserver/HttpServer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Net;
@@ -9,30 +10,39 @@ using System.Threading.Tasks;
 
 namespace httpserver
 {
-   public class HttpServer
-   {
-      public static readonly int DefaultPort = 8888;
-      public void StartServer()
-      {
-          //creates a server socket/listner/ server startup message using port 8888
-          TcpListener serverSocket = new TcpListener(DefaultPort);
-          serverSocket.Start();
-          Console.WriteLine("Server is activated");
+    public class HttpServer
+    {
+        public static readonly int DefaultPort = 8888;
+        private readonly EventLog _Log = new EventLog();
 
-          //Server stays open
-          while (true)
-         {
-            TcpClient connectionSocket = null;
-            using (connectionSocket)
+        public void StartServer()
+        {
+            //creates a server socket/listner/ server startup message using port 8888
+            _Log.Source = "HttpServer";
+            TcpListener serverSocket = new TcpListener(DefaultPort);
+            serverSocket.Start();
+            _Log.WriteEntry("Server startup.", EventLogEntryType.Information, 1);
+            Console.WriteLine("Server is activated");
+
+            //Server stays open
+            while (true)
             {
-               connectionSocket = serverSocket.AcceptTcpClient();
-               
-               var service = new HttpService(connectionSocket);
+                TcpClient connectionSocket = null;
+                using (connectionSocket)
+                {
+                    connectionSocket = serverSocket.AcceptTcpClient();
 
-               Task.Run(() => service.SocketHandler());
+                    var service = new HttpService(connectionSocket);
+
+                    Task.Run(() => service.SocketHandler());
+                }
+                Console.ReadLine();
+                if (Console.ReadLine() == "shutdown")
+                {
+                    serverSocket.Stop();
+                    _Log.WriteEntry("Server shutdown.", EventLogEntryType.Information, 4);
+                }
             }
-
-         }
-      }
-   }
+        }
+    }
 }
--- httpserver/HttpService.cs
+++ httpserver/HttpService.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Globalization;
 using System.IO;
 using System.Linq;
@@ -15,7 +16,8 @@ namespace httpserver
    {
       private TcpClient connectionSocket;
       private static readonly string RootCatalog = "c:/temp";
-
+      readonly EventLog _Log = new EventLog();
+      
       string Roottext { get; set; }
 
       public HttpService(TcpClient connectionSocket)
@@ -25,27 +27,26 @@ namespace httpserver
       public void SocketHandler()
       {
           //creates a Stream for the client to read from and write to
-          Stream ns = connectionSocket.GetStream();
+         Stream ns = connectionSocket.GetStream();
 
-          StreamReader sr = new StreamReader(ns);
-          StreamWriter sw = new StreamWriter(ns);
-          sw.AutoFlush = true; // enable automatic flushing
+         StreamReader sr = new StreamReader(ns);
+         StreamWriter sw = new StreamWriter(ns);
+         sw.AutoFlush = true; // enable automatic flushing
 
-          //input form the stream to another format
-          string message = sr.ReadLine();
-          Console.WriteLine(message);
+         //input form the stream to another format
 
-          string uritext = "";
-          //Puts the stream into an array
-          string[] words = message.Split(' ');
+          
+         string message = sr.ReadLine();
+          _Log.Source = "HttpServer";
+          _Log.WriteEntry("Client request: " + message, EventLogEntryType.Information, 2);
+         Console.WriteLine(message);
+
+         string uritext = "";
+         //Puts the stream into an array
+         string[] words = message.Split(' ');
 
          uritext = words[1].Replace("/", "/");
 
-         //sw.WriteLine("You requested " + uritext + "<br>");
-
-         uritext = uritext + ".txt";
-         Roottext = RootCatalog + uritext;
-          
          //TESTGET
          string code = "200 OK";
          const string illegalRequest = "400 Illegal request";
@@ -53,46 +54,60 @@ namespace httpserver
          const string illegalProtocol = "400 Illegal protocol";
          const string testMethodNotImplemented = "200 xxx";
          const string http10 = "HTTP/1.0";
-          
+
+         Roottext = RootCatalog + uritext;
+
          //If the file does not exist return error 404 Not Found
+
          if (!File.Exists(Roottext))
          {
-             code = "404 Not Found";
+            code = "404 Not Found";
          }
-
-         if (words[0] == "GET")
-            if (words[2] == "HTTP/1.0")
+         else
+         {
+            if (words[0] == "GET")
             {
-               sw.WriteLine(words[2] + " " + code);
-            }
-            else
-            {
-               if(words[2].Contains("/"))
-               { 
-               sw.WriteLine(http10 + " " + illegalProtocol);
+               if (words[2] == "HTTP/1.0" || words[2] == "HTTP/1.1")
+               {
+                  sw.WriteLine(http10 + " " + code);
                }
                else
                {
-                  sw.WriteLine(http10 + " " + illegalRequest);
+                  if (words[2].Contains("/"))
+                  {
+                     sw.WriteLine(http10 + " " + illegalProtocol);
+                  }
+                  else
+                  {
+                     sw.WriteLine(http10 + " " + illegalRequest);
+                  }
                }
             }
+            if (words[0] == "PLET")
+               sw.WriteLine(words[2] + " " + illegalMethod);
 
-         if (words[0] == "PLET")
-            sw.WriteLine(words[2] + " " + illegalMethod);
-            
-         if(words[0] == "POST")
-            sw.WriteLine(words[2] + " " + testMethodNotImplemented);
-
-         //string[] lines = File.ReadAllLines(Roottext);
-
-         //foreach (string line in lines)
-         //{
-         //   sw.WriteLine("\t" + line);
-         //}
+            if (words[0] == "POST")
+               sw.WriteLine(words[2] + " " + testMethodNotImplemented);
+            //UNIT TEST
+         }
+         sw.WriteLine("You requested " + uritext);
+         
+         string[] lines = File.ReadAllLines(Roottext);
+         foreach (string line in lines)
+         {
+            sw.WriteLine("\t" + line);
+         }
 
          sr.Close();
          sw.Close();
-         connectionSocket.Close();
+         connectionSocket.Close()
+         _Log.WriteEntry("Server response: ", EventLogEntryType.Information, 3);
+
+          
       }
+        
+      
+
+       
    }
 }
